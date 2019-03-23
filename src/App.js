612b5b36e5b174fb854e2249d92dacc1983c62e7
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-23 21:48:32 +0530

    Navbar, modal and Button Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+
+import Header from './components/Header/Header'; 
+import Modal from './components/Modal/Modal'
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    isAuth: true,
+    error: null
+  };
+
+  // LOGOUT HANDLER
+  logoutHandler(){
+    this.setState({ isAuth: false });
+  }
+
+
+  // ERROR HANDLER
+  errorHandler = msg => {
+    this.setState({ error: msg });
+  }
+
+
   render() {
     return (
       <div className="App">
-       
+        <Modal open={!!this.state.error} title="An Error Occurred" onClose={() => this.errorHandler(null)} ></Modal>
+        <Header authenticated={this.state.isAuth} onLogout={this.logoutHandler} />
       </div>
     );
   }
