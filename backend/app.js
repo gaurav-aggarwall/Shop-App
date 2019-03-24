fdd97c84bc84943b233c489c12b0d572971960f1
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-24 15:14:11 +0530

    Backend Inital Commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const express = require('express');
+const parser = require('body-parser');
+
+const productRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(parser.json());
+
+app.use('/images', express.static(path.join('backend/images')));
+
+// Set CORS headers so that the React SPA is able to communicate with this server
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use('/products', productRoutes);
+
+app.use('/', authRoutes);
+
+app.listen(3100, () => console.log('Server stared on 3100 Port'));
